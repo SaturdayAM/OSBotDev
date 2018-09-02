dea7ead203107fe33cd89d29feb1b14d3261f452
SaturdayAM <jwl62@georgetown.edu>
2018-09-02 17:05:01 -0400

    script skeleton

--- src/Woodcutter.java
+++ src/Woodcutter.java
@@ -1,9 +1,19 @@
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 
-@ScriptManifest(author = "Init", name="")
+/*
+    ScriptManifest annotation: required by main class to feed data to OSBot script selector.
+ */
+@ScriptManifest(author = "CaptFalcon", name="Woodcutting Script", info="A beginner script", version = 0.1,
+                logo= "https://png.icons8.com/color/180/runescape.png")
+public final class Woodcutter extends Script {
 
 
-public class Woodcutter {
 
+
+    @Override
+    public final int onLoop() throws InterruptedException { //core loop for running script
+
+        return 0; //the return value is the time between each onLoop call, in ms
+    }
 }
