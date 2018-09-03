fe65b65a0368c5cf5253f21d438be8528b7cf0a0
SaturdayAM <jwl62@georgetown.edu>
2018-09-03 13:01:36 -0400

    trying methods in script class

--- src/Woodcutter.java
+++ src/Woodcutter.java
@@ -6,9 +6,15 @@ import org.osbot.rs07.script.ScriptManifest;
  */
 @ScriptManifest(author = "CaptFalcon", name="Woodcutting Script", info="A beginner script", version = 0.1,
                 logo= "https://png.icons8.com/color/180/runescape.png")
+
+//Docs at:
+//https://osbot.org/api/org/osbot/rs07/script/Script.html
 public final class Woodcutter extends Script {
 
-
+    @Override
+    public final void onStart(){ //called on script start
+        log("Logged on start");
+    }
 
 
     @Override
@@ -16,4 +22,15 @@ public final class Woodcutter extends Script {
 
         return 0; //the return value is the time between each onLoop call, in ms
     }
+
+    @Override
+    public final int onExit(){
+        log("Logged on exit");
+    }
+
+    @Override
+    public final void onMessage(final Message message){
+        log("A message arrived in the chatbox: " + message.getMessage());
+    }
+
 }
