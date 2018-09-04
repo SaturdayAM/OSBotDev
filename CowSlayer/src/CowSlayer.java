5945b17b89368b38fbfb7632679024c149487ca4
SaturdayAM <jwl62@georgetown.edu>
2018-09-04 02:15:22 -0400

    bot now handles banking loot

--- CowSlayer/src/CowSlayer.java
+++ CowSlayer/src/CowSlayer.java
@@ -1,9 +1,12 @@
+import org.osbot.rs07.api.Bank;
 import org.osbot.rs07.api.filter.Filter;
 import org.osbot.rs07.api.map.Position;
 import org.osbot.rs07.api.map.Area;
+import org.osbot.rs07.api.map.constants.Banks;
 import org.osbot.rs07.api.EntityAPI;
 import org.osbot.rs07.api.map.constants.Banks;
 import org.osbot.rs07.api.model.Entity;
+import org.osbot.rs07.api.model.GroundItem;
 import org.osbot.rs07.api.ui.RS2Widget;
 import org.osbot.rs07.script.MethodProvider;
 import org.osbot.rs07.script.Script;
@@ -15,6 +18,7 @@ import org.osbot.rs07.api.model.Player;
 import org.osbot.rs07.api.filter.NameFilter;
 
 import java.util.function.BooleanSupplier;
+import java.util.concurrent.TimeUnit;
 
 /*
     ScriptManifest annotation: required by main class to feed data to OSBot script selector.
@@ -42,7 +46,7 @@ public final class CowSlayer extends Script {
     }
 
     private enum State { //Declare states
-        KILL, SLEEP;
+        KILL, LOOT, WAIT, BANKING;
     }
 
     private State getState(){ //returns the state
@@ -56,12 +60,20 @@ public final class CowSlayer extends Script {
 
         });
 
-        if(cow != null){
+        //States:
+        //Kill: if inventory not full
+        //Wait: If clicked attack
+        //Loot: Right after killing
+        //Bank: when full
+        if(cow != null && !inventory.isFull()){
             return State.KILL;
         }
-        return State.SLEEP;
+
+
+        return State.WAIT;
 
     }
+
 
     @Override
     public final int onLoop() throws InterruptedException { //core loop for running script
@@ -73,45 +85,76 @@ public final class CowSlayer extends Script {
             return random(500, 1000);
         }
 
-        NPC cow = getNpcs().closest(new Filter<NPC>() {
-            @Override
-            public boolean match(NPC npc) {
-                return npc != null && npc.exists() &&
-                        (npc.getName().equals("Cow")||npc.getName().equals("Cow calf")) && npc.isAttackable();
-            }
-        });
-
-        if(cow == null){
-            //chicken is null, wait for one to spawn
-            return random(500, 1000);
-        }
-
-        if(!cow.isOnScreen()){
-            camera.toEntity(cow);
-            return random(500, 1000);
-        }
-
-        cow.interact("Attack");
-
-        //Player position
-        Position playerPos = myPosition();
-
-        //Center of cow pen near falador
-        Position coopCenter = new Position(3032, 3305, 0);
-
-        Position randomCenter =  new Position(random(3029,3033), random(3302, 3307), 0);
-
-        //Rectangle Area spanning most of cow pen south of Falador. Params are corners x1y1, x2y2
-        Area cowCoop = new Area(3021, 3297, 3042, 3312);
-
-        if(!cowCoop.contains(playerPos)){ //if not in coop
-            getWalking().walk(coopCenter);
-        }
-
         //If leveled up, continue
         if(dialogues.inDialogue()){
             dialogues.clickContinue();
         }
+
+        //if inventory is full, go to bank and deposit hides
+        if(inventory.isFull()){
+            if(!Banks.FALADOR_EAST.contains(myPlayer().getPosition())){
+                getWalking().webWalk(Banks.FALADOR_EAST);
+            } else {
+                if(!getBank().isOpen()){
+                    getBank().open();
+                    new ConditionalSleep(2500, 3000){
+                        @Override
+                        public boolean condition(){
+                            return getBank().isOpen();
+                        }
+                    }.sleep();
+                } else{
+                    bank.depositAll("Cowhide"); //bank -> player bank
+                }
+            }
+            return(random(500, 1000));
+        } else{//If inventory is not full and not in cow area, go to cow area
+
+            //Player position
+            Position playerPos = myPosition();
+
+            //Center of cow pen near falador
+            Position coopCenter = new Position(3032, 3305, 0); //what looks to be center tile of spot
+            Position randomCenter =  new Position(random(3029,3033), random(3302, 3307), 0);
+
+            //Rectangle Area spanning most of cow pen south of Falador. Params are corners x1y1, x2y2
+            Area cowCoop = new Area(3021, 3297, 3042, 3312);
+
+            if(!cowCoop.contains(playerPos) && !combat.isFighting()){ //if not in pen and not under attack
+                getWalking().webWalk(randomCenter); //get around the center
+            }
+
+            //Find nearest attackable cow/ cow calf
+            NPC cow = getNpcs().closest(new Filter<NPC>() {
+                @Override
+                public boolean match(NPC npc) {
+                    return npc != null && npc.exists() &&
+                            (npc.getName().equals("Cow")||npc.getName().equals("Cow calf")) && npc.isAttackable();
+                }
+            });
+
+            if(cow == null){ // if cow is null, wait for one to spawn
+                return random(500, 1000);
+            }
+
+            if(!cow.isOnScreen()){ //rotate camera to have cow in it
+                camera.toEntity(cow);
+                return random(500, 1000);
+            }
+
+            cow.interact("Attack");
+
+            //Looting
+            GroundItem cowhide = groundItems.closest("Cowhide");
+            if((!inventory.isFull() && !myPlayer().isUnderAttack())){
+                cowhide.interact("Take");
+                return random(1500, 2000);
+            }
+
+        }
+
+
+
 
         return random(500, 1000); //the return value is the time between each onLoop call, in ms
     }
