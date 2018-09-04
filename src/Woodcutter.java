839c47bd2d029897d95557f48cd9178388b76244
SaturdayAM <jwl62@georgetown.edu>
2018-09-03 20:20:49 -0400

    finished initial working chicken killing script

--- src/Woodcutter.java
+++ src/Woodcutter.java
@@ -1,3 +1,4 @@
+import org.osbot.rs07.api.filter.Filter;
 import org.osbot.rs07.api.map.Position;
 import org.osbot.rs07.api.map.Area;
 import org.osbot.rs07.api.EntityAPI;
@@ -11,7 +12,7 @@ import org.osbot.rs07.utility.ConditionalSleep;
 import org.osbot.rs07.api.Combat;
 import org.osbot.rs07.api.model.NPC;
 import org.osbot.rs07.api.model.Player;
-
+import org.osbot.rs07.api.filter.NameFilter;
 
 import java.util.function.BooleanSupplier;
 
@@ -45,11 +46,52 @@ public final class Woodcutter extends Script {
     }
 
     private State getState(){ //returns the state
-        NPC
+
+        NPC chicken = getNpcs().closest(new Filter<NPC>(){
+            @Override
+            public boolean match(NPC npc){
+                return npc != null && npc.exists() &&
+                        npc.getName().equals("Chicken") && npc.isAttackable();
+            }
+
+        });
+
+        if(chicken != null){
+            return State.KILL;
+        }
+        return State.SLEEP;
+
     }
 
     @Override
     public final int onLoop() throws InterruptedException { //core loop for running script
+
+
+        //Referrenced from: https://osbot.org/forum/topic/70292-vags-chicken-blaster-first-script-ever/?do=findComment&comment=803046
+        if(myPlayer().getInteracting() != null){
+            //If player is interacting, return to stop checking until out of combat
+            return random(500, 1000);
+        }
+
+        NPC chicken = getNpcs().closest(new Filter<NPC>() {
+            @Override
+            public boolean match(NPC npc) {
+                return npc != null && npc.exists() &&
+                        npc.getName().equals("Chicken") && npc.isAttackable();
+            }
+        });
+
+        if(chicken == null){
+            //chicken is null, wait for one to spawn
+            return random(500, 1000);
+        }
+
+        if(!chicken.isOnScreen()){
+            camera.toEntity(chicken);
+            return random(500, 1000);
+        }
+
+        chicken.interact("Attack");
 
         //Player position
         Position playerPos = myPosition();
@@ -57,22 +99,17 @@ public final class Woodcutter extends Script {
         //Center of large chicken coop near sheep pen in Lumbridge
         Position coopCenter = new Position(3178, 3296, 0);
 
+        Position randomCenter =  new Position(random(3177,3179), random(3294, 3298), 0);
         //Rectangle Area spanning most of coop. Params are corners x1y1, x2y2
         Area chickenCoop = new Area(3172, 3290, 3183, 3302);
 
-        if(chickenCoop.contains(playerPos)){ //if in coop
-            log("in coop");
-            //get instance of NPCs
-            Entity chicken = getNpcs().closest("Chicken");
-            if(chicken != null && !Combat.isFighting()){
-                chicken.interact("Attack");
-            }
-        } else{
+        if(!chickenCoop.contains(playerPos)){ //if not in coop
+
             getWalking().walk(coopCenter);
         }
 
 
-        return 0; //the return value is the time between each onLoop call, in ms
+        return random(500, 1000); //the return value is the time between each onLoop call, in ms
     }
 
     @Override
