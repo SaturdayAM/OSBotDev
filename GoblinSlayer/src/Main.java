edf6e0a3c3127c36f24fcc257b11bd0c1dcfb896
SaturdayAM <jwl62@georgetown.edu>
2018-09-10 00:51:43 -0400

    testing lambda npc selection

--- GoblinSlayer/src/Main.java
+++ GoblinSlayer/src/Main.java
@@ -1,3 +1,4 @@
+import org.osbot.rs07.api.filter.Filter;
 import org.osbot.rs07.api.model.NPC;
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
@@ -5,24 +6,54 @@ import org.osbot.rs07.utility.ConditionalSleep;
 
 import java.awt.*;
 
-@ScriptManifest(name = "GoblinSlayer", info = "Kills lumbridge goblins", version =1.0 , logo ="" , author = "CaptFalcon")
+@ScriptManifest(name = "GoblinSlayer", info = "Kills lumbridge goblins", version =1.1 , logo ="" , author = "CaptFalcon")
 public class Main extends Script {
+
+    private String targetName = "Chicken";
+
     @Override
     public void onStart() throws InterruptedException {
         super.onStart();
-        log("does this work");
+        log("Initializing script");
     }
 
     @Override
     public int onLoop() throws InterruptedException {
 
-        NPC goblin = getNpcs().closest("Goblin");
+        if(getCombat().isFighting()){ //If we're fighting, loop
+            return random(500, 700);
+        }
 
-        if(goblin != null){ //if it exists
-            log("Goblin found");
-            if(!goblin.isVisible()){
-                getCamera().toEntity(goblin);
-                new ConditionalSleep(random(500,700), 100){
+        //Select target npc to fight
+
+        //Using a filter
+
+//        NPC targetNpc = getNpcs().closest(new Filter<NPC>() {
+//            @Override
+//            public boolean match(NPC npc) {
+//                //getMap() from map api. Checks if we can reach npc/
+//                return npc != null && npc.getName().equals(targetName) //is a chicken
+//                                   && npc.exists() //still exists
+//                                   && getMap().canReach(npc) //can be reached
+//                                   && npc.getHealthPercent() > 0 //not dying
+//                                   && npc.getInteracting() == null ; //not fighting somebody else
+//            }
+//        });
+
+
+        //With lambda notation
+        NPC targetNpc = getNpcs().closest(npc -> npc != null &&
+                                                 npc.getName().equals(targetName) &&
+                                                 npc.exists() &&
+                                                 getMap().canReach(npc) &&
+                                                 npc.getHealthPercent() > 0 &&
+                                                 npc.getInteracting() == null);
+
+        if(targetNpc != null){
+
+            if(!targetNpc.isVisible() ){ //Move camera to npc if needed, then pause
+                getCamera().toEntity(targetNpc);
+                new ConditionalSleep(random(300,500), 100){
                     @Override
                     public boolean condition(){
                         return false;
@@ -30,9 +61,7 @@ public class Main extends Script {
                 }.sleep();
             }
 
-            if(goblin.interact("Attack")){
-                log("We have attacked: " + goblin.getName());
-
+            if(targetNpc.interact("Attack")){ //If we've attacked it
                 //ConditionalSleep(int timeout, int sleepTime)
                 //sleepTime between condition checks. Default 25ms.
                 //Most actions will need much more time, reduce CPU load by increasing
@@ -48,13 +77,8 @@ public class Main extends Script {
                     }
                 }.sleep(); //call immediately, sleep
             }
-
-
-
         }
-
         return random(100, 300);
-
     }
 
 
@@ -63,8 +87,4 @@ public class Main extends Script {
         super.onExit();
     }
 
-    @Override
-    public void onPaint(Graphics2D iIIiiiiIiiII) {
-        super.onPaint(iIIiiiiIiiII);
-    }
 }
